2ba4cc79a42d123a6e7df689a2fe816c85c94ad7
yeemans <arthur.roth.07@gmail.com>
2023-11-19 20:58:49 -0800

    testing

--- components/flashcard/AddFlashcard.js
+++ components/flashcard/AddFlashcard.js
@@ -2,7 +2,7 @@ import { Input } from "@material-tailwind/react";
 function AddFlashcard() {
     return(
         <div className="flex w-72 flex-col items-end gap-6">
-            <Input size="lg" label="Front" />
+            <Input size="lg" label="Fronttttt" />
             <Input size="lg" label="Back" />
       </div>
     )
